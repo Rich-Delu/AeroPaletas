575c51c4d68134cd8d19234628a88ea653339e2d
Ricardo Delucio Fuentes <Ricardodelucioipn@gmail.com>
2025-05-22 11:47:25 -0600

    Actualización uso de Carpetas(Correccion del Error al leer los Archivo .json)

--- Grafos_AeroPaletas.py
+++ Grafos_AeroPaletas.py
@@ -11,10 +11,10 @@ class Aeropuertos:
         self.g = nx.DiGraph()
     
     def cargar_J(self):
-        with open("Grafo/Json/Info.json") as f:
+        with open("Json/Info.json") as f:
             self.info=json.load(f)
         
-        with open("Grafo/Json/Aero.json") as x:
+        with open("Json/Aero.json") as x:
             self.Aero=json.load(x) 
         
     def crear_nodos(self):
